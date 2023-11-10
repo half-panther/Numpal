b59b38fad1750487f8bd4a2bf6f8fb1f14e62bd5
half-panther <145088865+half-panther@users.noreply.github.com>
2023-11-10 02:42:53 +0000

    Create function to parse parenthesis within a string to be evaluated.

--- src/numpal.cpp
+++ src/numpal.cpp
@@ -1,66 +1,79 @@
-#include <cctype>
-#include <cstddef>
-#include <iostream>
 #include <string>
 #include <vector>
+
+/*
+ * Possible input examples
+ *
+ * 4 * 8 + 2
+ * 89 + (67 - 2) / 73
+ *
+ *(((((3)))
+ *
+ *
+ *
+ */
 
 namespace pal {
 class Token;
 
-void calculate(std::string calculatable) {
+Token parseParenthesisedToken(int&, const std::string &evalInput);
+Token parseDecimalToken(int &itr, const std::string &evalInput);
+Token parseToken(int &itr, const std::string &evalInput);
 
+struct Token {
+
+};
+
+void eval(std::string input) {
 }
 
-Token parseParenthesisedToken();
-Token parseDecimal();
-Token parseToken();
+//Any number of operator and operand pairs.
+inline Token parseParenthesisedToken(int &itr, const std::string &evalInput) {
+	int prths = 1, prthsStartPos = itr, prthsEndPos { };
+
+	while (prths) {
+		itr++;
+
+		(evalInput[itr] == '(') ? prths++ :
+		(evalInput[itr] == ')') ? prths-- : 1;
+	}
+	prthsEndPos = itr;
+
+	return Token { };
+}
+
+inline Token parseDecimalToken(int &itr, const std::string &evalInput) {
+	return Token { };
+}
+
+inline Token parseToken(int &itr, const std::string &evalInput) {
+	return Token { };
+}
 
 void parse(std::string input) {
 
-	std::vector tokens;
+	std::vector<Token> tokens;
+	int itr { };
 
 	switch (input[0]) {
 
 	case '(':
-		tokens.push_back(parseParenthesisedToken());
+		tokens.push_back(parseParenthesisedToken(itr, input));
 		break;
 
 	case '.':
-		tokens.push_back(parseDecimal());
+		tokens.push_back(parseDecimalToken(itr, input));
 		break;
 
 	case '0' ... '9':
-		tokens.push_back(parseToken());
+		tokens.push_back(parseToken(itr, input));
 		break;
 
 	default:
+		;
 
-		//TODO error - Invalid token
-
+		//TODO Error - Invalid token
 	}
-
 }
-
-inline Token parseParenthesisedToken() {
-
-	return "";
-}
-
-inline Token parseDecimal() {
-
-	return "";
-}
-
-inline Token parseToken() {
-
-	return "";
-}
-}
-
-int main() {
-
-	pal::calculate("4 + 2 + 6  s");
-
-	return 0;
 }
 
